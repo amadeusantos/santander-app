ebf52d647423909ecafc8313ac40324c72d41349
Amadeu Santos <amadeusantos31@gmail.com>
2023-09-18 16:19:22 -0300

    criado o AccountMenu

---
+++ src/components/AccountMenu/index.tsx
@@ -0,0 +1,120 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Button } from "../Button";
+import { Entypo } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import Pix from "../../assets/images/pix.png";
+import Transferir from "../../assets/images/transferir.png";
+import Pagar from "../../assets/images/pagar.png";
+
+export function AccountMenu() {
+  return (
+    <View>
+      <View
+        style={{
+          backgroundColor: "#ec0000",
+          paddingVertical: "16%",
+          paddingHorizontal: 12,
+        }}
+      >
+        <Text style={{ color: "#fff", fontSize: 18 }}>Olá, Mundo!</Text>
+        <Text style={{ color: "#fff", fontSize: 18, fontWeight: "bold" }}>
+          Ag 0000 Co 00000-0
+        </Text>
+      </View>
+      <View style={{ alignItems: "center", gap: 40 }}>
+        <Button
+          style={{
+            backgroundColor: "#fff",
+            marginTop: -32,
+            width: "90%",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingVertical: 12,
+            gap: 12,
+          }}
+        >
+          <Ionicons name="md-swap-horizontal" size={52} color="#ec0000" />
+          <Text>
+            Cadastre sua chave Pix e facilite suas transações. Clique aqui.
+          </Text>
+        </Button>
+        <Button
+          style={{
+            width: "90%",
+            backgroundColor: "#fff",
+            flexDirection: "row",
+            alignItems: "center",
+            padding: 12,
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 12 }}>
+            <MaterialIcons
+              name="account-balance-wallet"
+              size={28}
+              color="#ec0000"
+            />
+            <Text style={{ fontSize: 16 }}>Saldo disponivel</Text>
+          </View>
+          <Entypo name="chevron-thin-down" size={24} color="black" />
+        </Button>
+        <View
+          style={{
+            width: "100%",
+            paddingHorizontal: 18,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Button
+            style={{
+              width: "30%",
+              height: 90,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#fff",
+            }}
+          >
+            <Image
+              source={Pix}
+              style={{ width: 32, height: 32, tintColor: "#ec0000" }}
+            />
+            <Text style={{ color: "#6d6d6d" }}>Pix</Text>
+          </Button>
+          <Button
+            style={{
+              width: "30%",
+              height: 90,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#fff",
+            }}
+          >
+            <Image
+              source={Pagar}
+              style={{ width: 32, height: 32, tintColor: "#ec0000" }}
+            />
+            <Text style={{ color: "#6d6d6d" }}>Pagar</Text>
+          </Button>
+          <Button
+            style={{
+              width: "30%",
+              height: 90,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#fff",
+            }}
+          >
+            <Image
+              source={Transferir}
+              style={{ width: 32, height: 32, tintColor: "#ec0000" }}
+            />
+            <Text style={{ color: "#6d6d6d" }}>transferir</Text>
+          </Button>
+        </View>
+      </View>
+    </View>
+  );
+}
--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,21 +1,44 @@
 import { Text, View } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons'; 
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 import { Image } from "react-native";
 import Santander from "../../assets/images/santander.png";
 
 export function TopBar() {
-    return (
-        <View style={{ backgroundColor: "#ec0000", position:"absolute", top: "3.8%", flexDirection: "row", width: "100%", paddingVertical: "5%", paddingHorizontal:12, justifyContent: "space-between" }} >
-            <Ionicons name="menu-outline" size={30} color="#f0f0f0" />
-            <View style={{ flexDirection: "row", gap: 6, alignItems: "center", justifyContent: "center" }}>
-                <Image source={Santander} style={{ width: 24, height: 24 }} />
-                <Text style={{ fontSize: 20, color: "#f0f0f0" }}>Santander</Text>
-            </View>
-            <View style={{ flexDirection: "row", gap: 16 }}>
-                <Ionicons name="ios-chatbox-ellipses-outline" size={24} color="#f0f0f0" />
-                <FontAwesome5 name="bell" size={24} color="#f0f0f0" />
-            </View>
-        </View>
-    )
+  return (
+    <View
+      style={{
+        backgroundColor: "#ec0000",
+        position: "absolute",
+        top: 24,
+        flexDirection: "row",
+        width: "100%",
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        justifyContent: "space-between",
+        elevation: 1,
+      }}
+    >
+      <Ionicons name="menu-outline" size={30} color="#f0f0f0" />
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 6,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image source={Santander} style={{ width: 24, height: 24 }} />
+        <Text style={{ fontSize: 20, color: "#f0f0f0" }}>Santander</Text>
+      </View>
+      <View style={{ flexDirection: "row", gap: 16 }}>
+        <Ionicons
+          name="ios-chatbox-ellipses-outline"
+          size={24}
+          color="#f0f0f0"
+        />
+        <FontAwesome5 name="bell" size={24} color="#f0f0f0" />
+      </View>
+    </View>
+  );
 }
