e71b1291bf5966ec88c031f718bcf42e4deeb6a3
Amadeu Santos <amadeusantos31@gmail.com>
2023-09-18 16:17:46 -0300

    permitir import de imagens

---
+++ custom.d.ts
@@ -0,0 +1,16 @@
+declare module '*.jpg' {
+    const path: any;
+    export default path;
+}
+declare module '*.jpeg' {
+    const path: any;
+    export default path;
+}
+declare module '*.svg' {
+    const path: any;
+    export default path;
+}
+declare module '*.png' {
+    const path: any;
+    export default path;
+}
